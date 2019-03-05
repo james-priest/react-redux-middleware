2de71c0f740d3ca7c5a6f0793fbef28895d70054
James Priest <james-priest@outlook.com>
2019-03-05 14:49:34 -0800

    feat: Add Async fetch of Posts  with redux-thunk

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -16,3 +16,23 @@ export function foundBadWord(payload) {
     payload
   };
 }
+
+// export function getData() {
+//   return function(dispatch) {
+//     return fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then(response => response.json())
+//       .then(json => {
+//         dispatch({ type: "DATA_LOADED", payload: json });
+//       });
+//   };
+// }
+
+export function getData() {
+  // return async function(dispatch, getState) {
+  return async function(dispatch) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await response.json();
+    dispatch({ type: "DATA_LOADED", payload: json });
+    // console.log(getState().remoteArticles.slice(3, 4));
+  };
+}
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -3,12 +3,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware3 } from "../middleware/index";
+import thunk from "redux-thunk";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware3))
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware3, thunk))
 );
 
 // Comment out to remove state output to console
